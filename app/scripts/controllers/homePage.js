5b5f8eaf343db138a72e449b8c71850261a91be5
jackle91 <hieu6@gatech.edu>
2017-11-30 13:48:14 -0500

    fixing UI

--- app/scripts/controllers/homePage.js
+++ app/scripts/controllers/homePage.js
@@ -1,16 +1,20 @@
-'use strict';
+(function () {
+  'use strict';
 
-/**
- * @ngdoc function
- * @name uDirtyRatWebApp.controller:AboutCtrl
- * @description
- * # AboutCtrl
- * Controller of the uDirtyRatWebApp
- */
-angular.module('uDirtyRatWebApp')
-  .controller('homePageCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS'
-    ];
-  });
+  /**
+   * @ngdoc function
+   * @name uDirtyRatWebApp.controller:AboutCtrl
+   * @description
+   * # AboutCtrl
+   * Controller of the uDirtyRatWebApp
+   */
+  angular.module('uDirtyRatWebApp')
+    .controller('homePageCtrl', function () {
+      this.awesomeThings = [
+        'HTML5 Boilerplate',
+        'AngularJS'
+      ];
+      var vm = this;
+    });
+
+})();
--- app/scripts/controllers/registration.js
+++ app/scripts/controllers/registration.js
@@ -1,16 +1,28 @@
-'use strict';
+(function () {
+  'use strict';
 
-/**
- * @ngdoc function
- * @name uDirtyRatWebApp.controller:AboutCtrl
- * @description
- * # AboutCtrl
- * Controller of the uDirtyRatWebApp
- */
-angular.module('uDirtyRatWebApp')
-  .controller('registrationCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS'
-    ];
-  });
+  /**
+   * @ngdoc function
+   * @name uDirtyRatWebApp.controller:AboutCtrl
+   * @description
+   * # AboutCtrl
+   * Controller of the uDirtyRatWebApp
+   */
+  angular.module('uDirtyRatWebApp')
+    .controller('registrationCtrl', function () {
+
+      var vm = this;
+      this.awesomeThings = [
+        'HTML5 Boilerplate',
+        'AngularJS'
+      ];
+
+      vm.userName = '';
+      vm.password = '';
+
+      vm.submit = function () {
+        $location.path('/homePage');
+      };
+    });
+
+})();
